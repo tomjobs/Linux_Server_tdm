878701d52af9aa87a002cca91b82b935ccf94ad2
tomjobs <tomjobs2000@gmail.com>
2021-03-07 20:53:13 +0800

    First file

---
+++ myServer/myServer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myServer
+//
+//  Created by apple on 2021/3/7.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
